95a245e771200547552e28f8dfefff72aa1f3a06
551100kk <551100kk@gmail.com>
2016-08-02 17:37:03 +0800

    show card 1/2

--- static/js/cardsgame.js
+++ static/js/cardsgame.js
@@ -30,6 +30,21 @@ function showproblem(probid, runtime, element){
             console.log(result.prob);
             $(element).find('.show')[0].setAttribute('src', '/xblock/resource/cardsgame/public/img/allcards/allcards [www.imagesplitter.net]-' + (result.probid % 4) + '-' + parseInt(result.probid / 13) + '.png');
             $(element).find('.show').css('opacity', 0);
+            $(element).find('.description')[0].innerText = result.prob[0];
+            var template = '<a type="button" class="btn btn-default stu_ans" name="ansoptioncnt">option</a><br>';
+            for(var i = 0; i < result.prob[1].length; i++){
+                var newtag = template.replace(/optioncnt/g, i);
+                newtag = newtag.replace('option', result.prob[1][i]);
+                $(element).find('.option').append(newtag);
+            }
+            $(element).find('.stu_ans').bind('click', function(){
+                if(this.getAttribute('class') == 'btn btn-default stu_ans'){
+                    this.setAttribute('class', 'btn btn-success stu_ans');
+                }
+                else{
+                    this.setAttribute('class', 'btn btn-default stu_ans');
+                }
+            });
             appear(50, runtime, element);
         }
     }); 
@@ -50,7 +65,7 @@ function checkunsolve(runtime, element){
 }
 function main(runtime, element){
     checkunsolve(runtime, element);
-    $('.card').bind('click', function(){
+    $(element).find('.card').bind('click', function(){
         if(show_problem){
             $(element).find("span[id=message]").css('color','red');
             $(element).find("span[id=message]")[0].innerText = 'Please solve the current problem!';
